9eff69cf06394bbc73bf0e4af54ebab25cba7647
Kirk Easterson <kirk.easterson@gmail.com>
2020-04-15 10:50:44 +0200

    Added more events and also commands. The differences between the two are very little, but commands generally don't have output.

--- src/mininet_listener.py
+++ src/mininet_listener.py
@@ -32,7 +32,24 @@ class Observer:
 
 
 class MininetEvent(Enum):
-    HOST_CREATED = "New host created"
+    ADD_HOST = "New host created"
+    DEL_HOST = "Host deleted"
+    ADD_SWITCH = "New switch created"
+    DEL_SWITCH = "Switch deleted"
+    ADD_CONTROLLER = "New controller created"
+    DEL_CONTROLLER = "Controller deleted"
+    ADD_LINK = "New link created"
+    DEL_LINK = "Link deleted"
+    CONFIG_HOSTS = "Hosts configured"
+
+class MininetCommand(Enum):
+    CONFIG_HOSTS = "Hosts configured"
+    PING = "Ping sent"
+    PING_FULL = "Ping full sent"
+    PING_ALL = "Ping all sent"
+    PING_PAIR = "Ping pair sent"
+    PING_ALL_FULL = "Ping all full sent"
+    PING_PAIR_FULL = "Ping pair full sent"
 
 
 class EventHandler(Observable):
@@ -42,9 +59,17 @@ class EventHandler(Observable):
     def __init__(self):
         Observable.__init__(self)
 
+    # This event will be rewritten. I plan on creating methods for each event type. The
+    # params will be passed down to that method and the PTF code will be written there
     def create_event(self, event_type, info):
-        self.notify_observers(event_type,info.params,info.intfs,info.ports,info.nameToIntf)
+        self.notify_observers(event_type, str(info))
+        # The below works for anything that is not a Link. There should be events for
+        #   each type of event, and that will properly handle a Link. For now, the above
+        #   generic method is used.
+        # self.notify_observers(event_type,info.params,info.intfs,info.ports,info.nameToIntf)
 
+    def create_command(self, command_type):
+        self.notify_observers(command_type)
 
 class EventListener(Observer):
 
