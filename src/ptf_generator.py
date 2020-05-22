a3aca61cd91d482e7adf8bab661049f21044bd7f
Kirk Easterson <kirk.easterson@gmail.com>
2020-05-22 14:01:27 +0200

    Added methods for SAI switch

--- src/ptf_generator.py
+++ src/ptf_generator.py
@@ -26,15 +26,13 @@ class ptfGenerator:
         self.pings = pings
 
     def generatePreamble(self):
-        return """
-import time
+        return """import time
 import logging
 import ptf.dataplane as dataplane
 import sai_base_test
 from ptf.testutils import *
 from switch_sai_thrift.ttypes import  *
 from ptf.mask import Mask
-
 """
 
     def initializeVariables(self):
@@ -42,7 +40,6 @@ from ptf.mask import Mask
 switch_inited=0
 port_list = []
 table_attr_list = []
-
 """
 
     def generateClassDefinition(self):
@@ -51,6 +48,9 @@ class {className}(sai_base_test.SAIThriftDataplaneTest):
 """.format(className=self.className)
 
     def createPing(self, src, dst):
+        mac1=self.macs[src]
+        mac2=self.macs[dst]
+        ip_dst = self.ips[dst].split("/")[0]
         return """
         print(\"Sending packet from {src} to {dst}\")
         switch_init(self.client) # Need help with this
@@ -58,6 +58,15 @@ class {className}(sai_base_test.SAIThriftDataplaneTest):
         #port2 = \"port2\"
         mac1 = \"{mac1}\"
         mac2 = \"{mac2}\"
+        mac_action = 1
+
+        self.client.sai_thrift_create_vlan(vlan_id)
+        vlan_port1 = sai_thrift_vlan_port_t(port_id=port1, tagging_mode=0)
+        vlan_port2 = sai_thrift_vlan_port_t(port_id=port2, tagging_mode=0)
+        self.client.sai_thrift_add_ports_to_vlan(vlan_id, [vlan_port1, vlan_port2])
+
+        sai_thrift_create_fdb(self.client, vlan_id, mac1, port1, mac_action)
+        sai_thrift_create_fdb(self.client, vlan_id, mac2, port2, mac_action)
 
         pkt = simple_tcp_packet(eth_dst=\"{mac1}\",
                                 eth_src=\"{mac2}\",
@@ -66,16 +75,24 @@ class {className}(sai_base_test.SAIThriftDataplaneTest):
                                 ip_ttl=64)
 
         try:
+            # in tuple: 0 is device number, 2 is port number
+            # this tuple uniquely identifies a port
             send_packet(self, (0, 2), pkt)
             verify_packets(self, pkt, device_number=0, ports=[1])
+            # or simply
+            # send_packet(self, 2, pkt)
+            # verify_packets(self, pkt, ports=[1])
         finally:
-            # Some sort of cleanup
+            sai_thrift_delete_fdb(self.client, vlan_id, mac1, port1)
+            sai_thrift_delete_fdb(self.client, vlan_id, mac2, port2)
 
-""".format(src=src, dst=dst, mac1=self.macs[src], mac2=self.macs[dst], ip_dst=self.ips[dst])
+            self.client.sai_thrift_remove_ports_from_vlan(vlan_id, [vlan_port1, vlan_port2])
+            self.client.sai_thrift_delete_vlan(vlan_id)
+""".format(src=src, dst=dst, mac1=mac1, mac2=mac2, ip_dst=ip_dst)
 
     def generateRunTestMethod(self):
         result = """
-def runTest(self):
+    def runTest(self):
 """
         for ping in self.pings:
             (src, dst) = ping
